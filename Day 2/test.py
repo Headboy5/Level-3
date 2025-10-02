6b631e9c06faa475b5bcbc2b7a30dfed2a5ddae4
Headboy5 <stephenajclark@proton.me>
2025-10-02 10:46:21 +0100

    Activity 1

--- Day 2/test.py
+++ Day 2/test.py
@@ -1,4 +1,19 @@
+# I. Sentence with a horizontal tab
+# This sentence contains a tab character (\t) between two phrases.
+sentence_with_tab = "This is the first part\tand this is after the tab."
+print(sentence_with_tab)
+
+# ii. Sentence with a line break
+# This sentence contains a line break (\n) between two lines.
+sentence_with_linebreak = "This is the first line.\nThis is the second line."
+print(sentence_with_linebreak)
+
+# iii. Accept user input
+user_input = input("Enter something: ")
+print("You entered:", user_input)
+
 print('\\Insert a backslash character in a \\string.')
-print(' \t Inserts a horizontal tab')
-print(' \" Insert a double quote in a string e.g \"cat\" ')
-print(' \' Insert a single quote in a string  e.g \'dog\' ')
+print(' \t Inserts a horizontal tab')
+print(' " Insert a double quote in a string e.g "cat" ')
+print(' \' Insert a single quote in a string  e.g \'dog\' ')
+print("")
