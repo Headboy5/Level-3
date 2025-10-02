49d1b0530b8949382a076624cd63ecb26d058b9d
Headboy5 <stephenajclark@proton.me>
2025-10-02 10:51:00 +0100

    Technically it doesn't need the third comment

--- Day 2/test.py
+++ Day 2/test.py
@@ -4,7 +4,6 @@ print("This is the first part\tand this is after the tab.")
 #Sentence with a line break
 print("This is the first line.\nThis is the second line.")
 
-#Accept user input
 user_input = input("Enter something: ")
 print(f"You entered: {user_input}")
 
