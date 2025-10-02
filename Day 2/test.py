c954143b5dd2593886d2fb59ec51650631b57075
Headboy5 <stephenajclark@proton.me>
2025-10-02 11:14:54 +0100

    commented out weird bit

--- Day 2/test.py
+++ Day 2/test.py
@@ -7,10 +7,10 @@ print("This is the first line.\nThis is the second line.")
 user_input = input("Enter something: ")
 print(f"You entered: {user_input}")
 
-print('\\Insert a backslash character in a \\string.')
-print(' \t Inserts a horizontal tab')
-print(' " Insert a double quote in a string e.g "cat" ')
-print(' \' Insert a single quote in a string  e.g \'dog\' ')
+# print('\\Insert a backslash character in a \\string.')
+# print(' \t Inserts a horizontal tab')
+# print(' " Insert a double quote in a string e.g "cat" ')
+# print(' \' Insert a single quote in a string  e.g \'dog\' ')
 
 #Two modes of running Python:
 #   Interactive Mode: Type commands directly into the interpreter and get immediate results.
