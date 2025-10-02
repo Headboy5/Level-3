36af4637388c9d4598e74286875b35dff0aab5f5
Headboy5 <stephenajclark@proton.me>
2025-10-02 10:56:08 +0100

    perfect

--- Day 2/test.py
+++ Day 2/test.py
@@ -11,4 +11,3 @@ print('\\Insert a backslash character in a \\string.')
 print(' \t Inserts a horizontal tab')
 print(' " Insert a double quote in a string e.g "cat" ')
 print(' \' Insert a single quote in a string  e.g \'dog\' ')
-test
