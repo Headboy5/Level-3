b072139275338520894a00137bef4ecaaaf94269
Headboy5 <stephenajclark@proton.me>
2025-10-03 14:09:23 +0100

    a

--- Day 3/main.py
+++ Day 3/main.py
@@ -1,1 +1,16 @@
-test
+example_string = "Hello, world!"
+example_float = 3.14
+example_int = 42
+example_complex = 2j
+example_dict = {"name": "Alice", "age": 25}
+example_range = range(6,20)
+example_tuple = (1, 2, 3)
+example_bool = True
+example_list = [1, 2, 3, 4]
+example_set = {1, 2, 3}
+# Sequence Types: 	list, tuple, range
+# Mapping Type: 	dict
+# Set Types: 	set, frozenset
+# Boolean Type: 	bool
+# Binary Types: 	bytes, bytearray, memoryview
+# None Type: 	NoneType
