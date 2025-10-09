a08debfa6a8822e6eaf992394487f58c67ec6329
Headboy5 <stephenajclark@proton.me>
2025-10-09 11:05:26 +0100

    repeatedPrint

--- Week 3/Thursday - Programming/main.py
+++ Week 3/Thursday - Programming/main.py
@@ -12,6 +12,13 @@ def divideFunction(a, b):
 def addNames(name1, name2):
     print(f"My name is {name1} {name2}.")
 
+def squareFunction(x):
+    return x**2
+
+def repeatedPrint(msg, n):
+    for _ in range(n):
+        print(msg)
+
 def main():
     first_number = 2
     second_number = 3
@@ -21,6 +28,11 @@ def main():
     print(result)
     # greet("Kate")
     # greet("John", "Good evening!")
+    addNames("John", "Doe")
+    toSquare = 5
+    squared_value = squareFunction(toSquare)
+    print(f"The square of {toSquare} is {squared_value}.")
+    repeatedPrint("Hello World!", 3)
 
 if __name__ == "__main__":
     main()
