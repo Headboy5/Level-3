37f2898d5aa0ae85dff21a9d950c674d1e9f8dee
Headboy5 <stephenajclark@proton.me>
2025-10-09 10:58:46 +0100

    addnames + dividefunction

--- Week 3/Thursday - Programming/main.py
+++ Week 3/Thursday - Programming/main.py
@@ -1,11 +1,26 @@
 def addFunction(a,b):
     return a + b
 
+# def greet(name, msg="Good morning!"):
+#     print("Hello", name + ', ' + msg)
+
+def divideFunction(a, b):
+    if b == 0:
+        return "Error: Division by zero"
+    return a / b
+
+def addNames(name1, name2):
+    print(f"My name is {name1} {name2}.")
+
 def main():
     first_number = 2
     second_number = 3
-    result = addFunction(first_number, second_number)
+    # result = addFunction(first_number, second_number)
+    # print(result)
+    result = divideFunction(first_number, second_number)
     print(result)
+    # greet("Kate")
+    # greet("John", "Good evening!")
 
 if __name__ == "__main__":
     main()
