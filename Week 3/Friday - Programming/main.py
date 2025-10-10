4cb090aff6f1fc3121f5aa47efda1f9015eea1e6
Headboy5 <stephenajclark@proton.me>
2025-10-10 14:11:51 +0100

    greaterThan3 + stayAtHome

---
+++ Week 3/Friday - Programming/main.py
@@ -0,0 +1,17 @@
+#==ACTIVITY 1==#
+def greaterThan3(x):
+    if x > 3:
+        print(f"{x} is greater than 3")
+    else:
+        print(f"{x} is not greater than 3")
+
+def stayAtHome(x):
+    if x > 30:
+        print("Stay at home")
+    else:
+        print("I am out and about")
+
+greaterThan3(5)
+greaterThan3(2)
+stayAtHome(35)
+stayAtHome(25)
