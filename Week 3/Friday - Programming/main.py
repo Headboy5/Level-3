a2026e69edb4e5ae069e9d69e438db73e27249e4
Headboy5 <stephenajclark@proton.me>
2025-10-10 14:35:00 +0100

    activity 1

--- Week 3/Friday - Programming/main.py
+++ Week 3/Friday - Programming/main.py
@@ -5,13 +5,28 @@ def greaterThan3(x):
     else:
         print(f"{x} is not greater than 3")
 
-def stayAtHome(x):
-    if x > 30:
+def stayAtHome(temp):
+    if temp > 30:
         print("Stay at home")
     else:
         print("I am out and about")
+    #image.png
 
-greaterThan3(5)
-greaterThan3(2)
-stayAtHome(35)
-stayAtHome(25)
+def compareTwoVariables(a, b):
+    if a > b:
+        print(f"a: {a} is greater than b: {b}")
+    elif a == b:
+        print(f"a: {a} is equal to b: {b}")
+    else:
+        print(f"a: {a} is less than b: {b}")
+
+def main():
+    greaterThan3(5)
+    greaterThan3(2)
+    stayAtHome(35)
+    stayAtHome(25)
+    compareTwoVariables(5, 3)
+    compareTwoVariables(3, 3)
+
+if __name__ == "__main__":
+    main()
