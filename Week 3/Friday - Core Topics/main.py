3923508db94cc381f378e2251dfdd35b0446bea7
Headboy5 <stephenajclark@proton.me>
2025-10-10 11:37:49 +0100

    Yea I really don't get this

--- Week 3/Friday - Core Topics/main.py
+++ Week 3/Friday - Core Topics/main.py
@@ -11,8 +11,15 @@ print(advertising.head())
 print(advertising.shape)
 print(advertising.info())
 print(advertising.describe())
+
 sns.pairplot(advertising, x_vars=['TV', 'Newspaper', 'Radio'], y_vars='Sales',size=4, aspect=1, kind='scatter')
 # plt.show()
-
 sns.heatmap(advertising.corr(), cmap="YlGnBu", annot = True)
 plt.show()
+
+X = advertising['TV']
+y = advertising['Sales']
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.7, test_size = 0.3, random_state = 100)
+print(X_train.head())
+print(y_train.head())
