03053c2a18072abd4dc783eb15df8287fb4c9df8
Headboy5 <stephenajclark@proton.me>
2025-10-10 14:57:53 +0100

    Activity 2

--- Week 3/Friday - Programming/test.py
+++ Week 3/Friday - Programming/test.py
@@ -1,2 +1,12 @@
-for number in range(20):
+count = 1
+
+while True:
+    value = input("Integer, please [q to quit]: ")
+    if value == 'q':
+        break
+    number = int(value)
     print(number)
+    if number % 2 == 0:
+        continue
+    print(number, "squared is", number * number)
+    count += 1
