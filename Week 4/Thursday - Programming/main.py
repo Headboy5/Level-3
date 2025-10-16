4ee04cc10328bd2467b8e8c3c6e08e3b1bb4c6e9
Headboy5 <stephenajclark@proton.me>
2025-10-16 10:14:51 +0100

    Activity 1

---
+++ Week 4/Thursday - Programming/main.py
@@ -0,0 +1,40 @@
+# ==ACTIVITY 1==
+
+# Class: Smartphone
+
+# Attributes
+# brand
+# battery_level
+# storage_capacity
+
+# Methods
+# take_photo()
+# make_call()
+# install_app()
+
+
+# Class: Candle
+
+# Attributes
+# color
+# scent
+# burn_time
+
+# Methods
+# light()
+# extinguish()
+# melt()
+
+
+# Class: Backpack
+
+# Attributes
+# size
+# color
+# weight_capacity
+
+# Methods
+# open()
+# close()
+# add_item()
+
