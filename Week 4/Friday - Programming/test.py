c85a92408a23f2a3ed2207b83a15625f0ec6a527
Headboy5 <stephenajclark@proton.me>
2025-10-24 14:34:00 +0100

    But why?

--- Week 4/Friday - Programming/test.py
+++ Week 4/Friday - Programming/test.py
@@ -1,16 +1,10 @@
-# minute = 59
-# hour = 37
-# celsius = 78.56
-# horses = 33
-# listofitems = 120
-
-# print(minute / 60)
-# print(celsius * 60)
-# print(horses // 12)
-# print(2 * (3 - 1))
-# print((1 + 1) ** (5 - 2))
-# print((minute * 100) / 60)
-# print(2 ** 1 + 1)
-# print(2 / 3 - 1)
-# print(minute * 100 / 60)
-# print(not True False)
+x1 = 5
+y1 = 5
+x2 = 'Hello'
+y2 = 'Hello'
+x3 = [1,2,3]
+y3 = [1,2,3]
+print(x1 is not y1)  # false
+print(x2 is y2)      # true
+print(x3 is y3)      # false
+print(x3 == y3)      # true
