76bbe9e299016218b048edfd623507895de252a6
Headboy5 <stephenajclark@proton.me>
2025-10-27 10:21:19 +0000

    idk

---
+++ Week 4/Thursday - Programming/blah.cpp
@@ -0,0 +1,16 @@
+// #include <iostream>
+// #include <string>
+
+// class BankAccount {
+//     private:
+//         double balance;
+//     public:
+//         std::string accountHolderName;
+        
+
+
+// }
+        
+// int main() {
+    
+// }
